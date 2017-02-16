a6a2ac42a1be4581ce21dfa8e4bfccd847443611
Davide Santangelo <davidesantangelo@Davides-MacBook-Pro.local>
2017-02-16 14:51:34 +0100

    fix repo url

--- src/Components/RepoList.js
+++ src/Components/RepoList.js
@@ -7,7 +7,7 @@ const RepoList = props => {
   const results = props.data;
   let repos;
   if (results.length) {
-    repos = results.map(repo => <Repo name={repo.name} url={repo.url} description={repo.description} key={repo.id} />);    
+    repos = results.map(repo => <Repo name={repo.name} url={repo.html_url} description={repo.description} key={repo.id} />);    
   } else {
     repos = <NoRepos />
   }
