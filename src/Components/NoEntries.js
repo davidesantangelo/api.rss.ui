446dec43b26920f41433432260d200bf5a6972ab
Davide Santangelo <davide.santangelo@gmail.com>
2019-05-13 11:06:58 +0200

    I'm feeling lucky

--- src/Components/NoEntries.js
+++ src/Components/NoEntries.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const NoEntries = props => (
-  <div>
-    Your search for - <strong>{props.query}</strong> - did not match any documents
-    <p><br></br><a href="/?q=github&page=1"><button type="button" className="btn btn-secondary">Back</button></a></p> 
-  </div>
-);
+const NoEntries = props => {
+  return(
+    <div>
+      Your search for - <strong>{props.query}</strong> - did not match any documents
+      <p><br></br><a href="/"><button type="button" className="btn btn-secondary">Back</button></a></p> 
+    </div>
+  );
+}
 
 export default NoEntries;
--- src/Components/NoQuery.js
+++ src/Components/NoQuery.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
-const NoQuery = props => (
-  <div>
-    <h5>search entries or whatever you want to search.</h5>
-    <p><br></br><a href="/?q=news&page=1"><button type="button" className="btn btn-secondary">Try search 'news'</button></a></p> 
-  </div>
+const NoQuery = props => {
+
+  let queries = [
+    'news',
+    'sport',
+    'dev',
+    'facebook',
+    'youtube',
+    'amazon',
+    'instagram',
+    'weather',
+    'finance',
+    'entertainment'
+  ]
+
+  let query = queries[Math.floor(Math.random() * queries.length)];
+  let url = "/?q=" + query + "&page=1"
+
+  return (
+    <div>
+      <h5>search entries or whatever you want to search.</h5>
+      <p><br></br><a href={url}><button type="button" className="btn btn-secondary">I'm feeling lucky</button></a></p> 
+    </div>
+  );
   
-);
+}
 
 export default NoQuery;
