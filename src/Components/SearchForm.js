5bbb0f3714e452de9b20ade7efac190d7fb7e539
Davide Santangelo <davide.santangelo@gmail.com>
2019-05-15 10:15:51 +0200

    cleanup

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import queryString from 'query-string'
 
 export default class SearchForm extends Component {
   
-  state = {
-    searchText: ''
+  constructor(props) {
+    super(props);
+    let parsedQuery = queryString.parse(window.location.search);
+    let q = parsedQuery.q
+
+    this.state = {value: q};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
   onSearchChange = e => {
-    this.setState({ searchText: e.target.value });
+    this.setState({ value: e.target.value });
   }
 
   handleSubmit = e => {
@@ -15,11 +27,12 @@ export default class SearchForm extends Component {
     this.props.onSearch(this.query.value, 1);
   }
   
-  render() {  
+  render() {
     return (
       <form className="search-form" onSubmit={this.handleSubmit} >
         <label className="is-hidden" htmlFor="search">Search</label>
         <input type="text"
+               value={this.state.value}
                onChange={this.onSearchChange}
                name="search" 
                ref={(input) => this.query = input}
