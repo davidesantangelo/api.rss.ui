84d7bcd46f8c9ff8c980d5a76b18eb4b31caf39e
Davide Santangelo <davide.santangelo@gmail.com>
2019-11-21 17:29:38 +0100

    add blackfriday button

--- src/Components/NoQuery.js
+++ src/Components/NoQuery.js
@@ -25,7 +25,8 @@ const NoQuery = props => {
   return (
     <div>
       <h5>Search entries or whatever you want to search (news, sports, economy). For developers <a target="_BLANK" rel="noopener noreferrer" href="https://github.com/davidesantangelo/feedi">API</a>.</h5>
-      <p><br></br><a href={url}><button type="button" className="btn btn-primary">choose for me <i className="fas fa-random"></i></button></a></p> 
+      <p><br></br><a href={url}><button type="button" className="btn btn-primary">choose for me <i className="fas fa-random"></i></button></a> <a href="/?q=blackfriday"><button type="button" className="btn btn-warning">black friday rss<i className="fas fa-shopping-cart"></i></button></a></p> 
+      <p></p> 
     </div>
   );
   
