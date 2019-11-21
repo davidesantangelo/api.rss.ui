2d53e0b5341c7d53db9de6c1c5bb4b2379c4539f
Davide Santangelo <davide.santangelo@gmail.com>
2019-11-21 17:22:45 +0100

    cleanup

--- src/Components/NoQuery.js
+++ src/Components/NoQuery.js
@@ -25,7 +25,7 @@ const NoQuery = props => {
   return (
     <div>
       <h5>Search entries or whatever you want to search (news, sports, economy). For developers <a target="_BLANK" rel="noopener noreferrer" href="https://github.com/davidesantangelo/feedi">API</a>.</h5>
-      <p><br></br><a href={url}><button type="button" className="btn btn-info">choose for me <i className="fas fa-random"></i></button></a></p> 
+      <p><br></br><a href={url}><button type="button" className="btn btn-primary">choose for me <i className="fas fa-random"></i></button></a></p> 
     </div>
   );
   
