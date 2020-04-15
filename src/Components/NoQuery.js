dc71feff1ae68ce1c74bcf1de6cba5ba3c2ca9d5
Davide Santangelo <davide.santangelo@gmail.com>
2020-04-15 15:18:30 +0200

    Add sources button

--- src/Components/NoQuery.js
+++ src/Components/NoQuery.js
@@ -23,7 +23,8 @@ const NoQuery = props => {
       <p>
       <h5>Fast, Awesome, Clean and Powerful RSS search engine, browse through over <strong>800,000</strong> RSS posts. </h5>
       </p>
-      <a className="btn btn-dark" href="https://github.com/davidesantangelo/feedi" target="_BLANK">RESTful API <i className="fas fa-code"></i></a>
+      <a className="btn btn-dark" href="https://github.com/davidesantangelo/feedi" target="_BLANK">RESTful API <i className="fas fa-code"></i></a>  &nbsp;
+      <a href="https://docs.google.com/spreadsheets/d/1mVeA-tzWNTypBTWy0bgijKYrc3OkJ8VX1uKOhC9aKnk/edit#gid=0" target="_BLANK" className="btn btn-secondary">Sources <i className="fas fa-share-alt"></i></a>
     </div>
   );
   
